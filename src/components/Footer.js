565c83869025328f059199176ae383ffbc5e94cd
tagener-noisu <op4.renegat@gmail.com>
2018-10-18 09:53:00 +0600

    Render new image on preview click

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,14 @@ import Preview from "./Preview"
 export default class Footer extends React.Component {
 	render() {
 		return <div id="gallery-footer">{
-			this.props.images.map(i => (
-				<Preview preview={i.preview} url={i.url} />
-			))
+			this.props.images.map(i => {
+				const [key] = i.url.match(/[^\/]+$/);
+				return <Preview
+					preview={i.preview}
+					url={i.url}
+					key={key}
+					onClick={this.props.updateCurrent} />
+			})
 		}</div>;
 	}
 }
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -3,7 +3,10 @@ import React from "react";
 export default class Preview extends React.Component {
 	render() {
 		const style = {backgroundImage: `url("${this.props.preview}")`};
-		return <div className="gallery-preview" style={style} >
-		</div>;
+		const onClick = () => { this.props.onClick(this.props.url); };
+		return <div
+			className="gallery-preview"
+			style={style}
+			onClick={onClick} />
 	}
 }

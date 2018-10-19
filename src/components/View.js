b0b8ec4481b87af82081b2a70f7448a0a05165fa
tagener-noisu <op4.renegat@gmail.com>
2018-10-19 13:40:56 +0600

    Add webm\mp4 support

--- src/components/View.js
+++ src/components/View.js
@@ -1,8 +1,22 @@
 import React from "react";
 
+function Image(props) {
+	const style = {backgroundImage: `url("${props.url}")`};
+	return <div id="gallery-view" style={style} />;
+}
+
+function Video(props) {
+	return <div id="gallery-view">
+		<video src={props.url} autoPlay="1" controls="1" loop="1" />
+	</div>;
+}
+
 export default class View extends React.Component {
 	render() {
-		const style = {backgroundImage: `url("${this.props.url}")`};
-		return <div id="gallery-view" style={style} ></div>;
+		const {url} = this.props;
+		if (url && url.match(/\.(mp4|webm)$/))
+			return <Video url={url} />
+		else
+			return <Image url={url} />
 	}
 }
